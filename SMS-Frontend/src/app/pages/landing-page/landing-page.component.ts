7c32a520e38d8c82fc5797eae204bf46c4ba2de1
KushanRandeepa <kushanrandeepa55@gmail.com>
2025-07-19 12:12:50 +0530

    angular material

--- SMS-Frontend/src/app/pages/landing-page/landing-page.component.ts
+++ SMS-Frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
