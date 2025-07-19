38dd1f81eef78698f99cd3cea7686f5a446c56e7
KushanRandeepa <kushanrandeepa55@gmail.com>
2025-07-19 23:53:26 +0530

    landing page demo done

--- SMS-Frontend/src/app/pages/landing-page/landing-page.component.ts
+++ SMS-Frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [RouterLink, MatButtonModule, MatIconModule],
+  imports: [RouterLink, MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
-
 }
