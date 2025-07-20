ed168d54ad64bac5d8c576f21ae3e155c6ce4531
KushanRandeepa <kushanrandeepa55@gmail.com>
2025-07-20 10:59:21 +0530

    login /signup v1

--- SMS-Frontend/src/app/pages/login-page/login-page.component.ts
+++ SMS-Frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [MatFormFieldModule, MatIconModule, MatInputModule , MatButtonModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-
+ hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 }
--- SMS-Frontend/src/app/pages/signup-page/signup-page.component.ts
+++ SMS-Frontend/src/app/pages/signup-page/signup-page.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component , inject} from '@angular/core';
 import { IDeactivateGuard } from '../../services/guards/DeactivateGuard-service';
+
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-signup-page',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, MatInputModule, MatFormFieldModule, MatStepperModule, MatButtonModule ],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.css'
 })
@@ -13,5 +19,16 @@ export class SignupPageComponent implements IDeactivateGuard {
   canDeactivate(): boolean {
    return confirm(' Are you sure you want to leave .');
   }
+
+   private _formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this._formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+  });
+  secondFormGroup = this._formBuilder.group({
+    address: ['', Validators.required],
+  });
+  isLinear = false;
 
 }
